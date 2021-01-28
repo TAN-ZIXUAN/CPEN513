84f28095d603f96266e7c66fb5316937e3c43780
TAN-ZIXUAN <56602039+TAN-ZIXUAN@users.noreply.github.com>
2021-01-27 20:45:28 -0800

    Update window.py
    grid file parsed correctly

--- Assignment 1/window.py
+++ Assignment 1/window.py
@@ -4,7 +4,14 @@ from pygame_gui.elements import UIButton
 from pygame_gui.windows import UIFileDialog
 from pygame_gui.core.utility import create_resource_path
 
-WIDTH = 800
+WIDTH = 1000
+HEIGHT = 500
+NUM_X = 12
+NUM_Y = 9
+
+SIZE_X = 1000 / NUM_X
+SIZE_Y = 500 / NUM_Y
+
 #colors
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -18,23 +25,33 @@ GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
 class Window:
-    def __init__(self, row, col, width, total_rows):
+    def __init__(self):
         pygame.init()
         
-        self.row = row
-        self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.colour = WHITE
-        self.width = width
-        self.total_rows = total_rows
-        self.neighbours = []
+        # self.row = row
+        # self.col = col
+        # self.x = row * width
+        # self.y = col * width
+        # self.colour = WHITE
+        # self.width = width
+        # self.height = height
+        # self.total_rows = total_rows
+        # self.neighbours = []
+        # self.sources = []
+        # self.sinks = []
+        self.num_x = 0
+        self.num_y = 0
+        self.num_obs = 0 # number of obstacles
+        self.obs = []
+        self.num_wires = 0
+        self.wire2source ={}
+        self.wire2sink = {}
 
         
 
         pygame.display.set_caption("Assignment 1")
-        self.window_surface = pygame.display.set_mode((WIDTH, WIDTH))
-        self.ui_manager = pygame_gui.UIManager((WIDTH, WIDTH), "theme.json")
+        self.window_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.ui_manager = pygame_gui.UIManager((WIDTH, HEIGHT), "theme.json")
         self.background = pygame.Surface((WIDTH, WIDTH))
         self.background.fill(self.ui_manager.ui_theme.get_colour('dark_bg'))
 
@@ -52,14 +69,21 @@ class Window:
         self.clock = pygame.time.Clock()
         self.is_running = True
     
-    def get_pos(self):
-        return self.row, self.col
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
-
-    def update_neighbours(self, grid):
+    # def get_pos(self):
+    #     return self.row, self.col
         
+
+    # def make_obstacle(self):
+    #     self.colour = BLUE
+    # def make_path(self):
+    #     self.colour = PURPLE
+    # def draw(self, window):
+    #     pygame.draw.rect(window, self.colour, (self.x, self.y, self.width, self.width))
+
+    # def update_neighbours(self, grid):
+    #     self.neighbours = []
+
+
 
     
 
@@ -88,12 +112,41 @@ class Window:
                     file_path = create_resource_path(event.text)
                     with open(file_path, 'r') as file:
                         for line in file.readlines():
-                            self.loaded_file.append(line.strip())
+                            self.loaded_file.append((line.strip().split())) # [['40', '20'], ['101'], ['25', '1'], ['26', '1'], ['27', '1']
                     print("-----------------------file loaded ---------------------")
                     print(self.loaded_file)
                     print("--------------------------------------------------------")
-                    # TODO add label display selected file name in window
+                    # TODO add label to display selected file name in window
 
+                    self.num_x, self.num_y = int(self.loaded_file[0][0]), int(self.loaded_file[0][1])
+                    self.num_obs = int(self.loaded_file[1][0])
+                    for i in range(self.num_obs):
+                        self.obs.append((int(self.loaded_file[2 + i][0]), int(self.loaded_file[2 + i][1])))
+
+                    self.num_wires = int(self.loaded_file[2 + self.num_obs][0])
+                    print("num of wires", self.num_wires)
+                    for i in range(self.num_wires):
+                        print("wire", i)
+                        print(self.loaded_file[2 + self.num_obs + 1 + i])
+                        num_pins = int(self.loaded_file[2 + self.num_obs + 1 + i][0])
+                        print("num of pins", num_pins)
+                        self.wire2source[i] = (int(self.loaded_file[2 + self.num_obs + 1 + i][1]), int(self.loaded_file[2 + self.num_obs + 1 + i][2]))
+                        
+                        for j in range(num_pins - 1):
+                            self.wire2sink[i] = (int(self.loaded_file[2 + self.num_obs + 1 + i][3 + j]), int(self.loaded_file[2 + self.num_obs + 1 + i][4 + j]))
+                    
+
+                    print("-----------------------num_x, num_y --------------------")
+                    print(self.num_x, self.num_y)
+                    print("--------------------------------------------------------")
+
+                    print("-----------------------wire 2 source -------------------")
+                    print(self.wire2source)
+                    print("--------------------------------------------------------")
+
+                    print("-----------------------wire 2 sink ---------------------")
+                    print(self.wire2sink)
+                    print("--------------------------------------------------------")
 
                 if (event.type == pygame.USEREVENT and
                     event.user_type == pygame_gui.UI_WINDOW_CLOSE and
